10e79d920255e0172d7f36e589c9ecd91bbc4f5f
Jonas Bautzmann <joba@spreadshirt.net>
2018-07-06 13:07:47 +0200

    updated db schema

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -5,35 +5,12 @@ CREATE TABLE `settings` (
 	ENGINE = InnoDB
 	DEFAULT CHARSET = utf8;
 
-CREATE TABLE `controller` (
-	`controller_id` INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	`hardware_id`   VARCHAR(255) NOT NULL,
-	`location_id`   INT          NOT NULL,
-	`settings_id`   INT          NOT NULL,
-	`date_created`  TIMESTAMP(3) NOT NULL             DEFAULT CURRENT_TIMESTAMP(3),
-	`date_modified` TIMESTAMP(3) NOT NULL             DEFAULT CURRENT_TIMESTAMP(3)
-	ON UPDATE CURRENT_TIMESTAMP(3),
-	FOREIGN KEY (settings_id) REFERENCES settings (settings_id)
-)
-	ENGINE = InnoDB
-	DEFAULT CHARSET = utf8;
-
 CREATE TABLE `unit` (
 	`unit_id`             INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	`name`                VARCHAR(255) NOT NULL,
 	`unit_of_measurement` VARCHAR(255) NOT NULL,
 	`date_created`        TIMESTAMP(3) NOT NULL             DEFAULT CURRENT_TIMESTAMP(3),
 	`date_modified`       TIMESTAMP(3) NOT NULL             DEFAULT CURRENT_TIMESTAMP(3)
-	ON UPDATE CURRENT_TIMESTAMP(3)
-)
-	ENGINE = InnoDB
-	DEFAULT CHARSET = utf8;
-
-CREATE TABLE `category` (
-	`category_id`   INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	`name`          VARCHAR(255) NOT NULL,
-	`date_created`  TIMESTAMP(3) NOT NULL             DEFAULT CURRENT_TIMESTAMP(3),
-	`date_modified` TIMESTAMP(3) NOT NULL             DEFAULT CURRENT_TIMESTAMP(3)
 	ON UPDATE CURRENT_TIMESTAMP(3)
 )
 	ENGINE = InnoDB
@@ -50,6 +27,43 @@ CREATE TABLE `location` (
 	ENGINE = InnoDB
 	DEFAULT CHARSET = utf8;
 
+CREATE TABLE `category` (
+	`category_id`   INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	`name`          VARCHAR(255) NOT NULL,
+	`date_created`  TIMESTAMP(3) NOT NULL             DEFAULT CURRENT_TIMESTAMP(3),
+	`date_modified` TIMESTAMP(3) NOT NULL             DEFAULT CURRENT_TIMESTAMP(3)
+	ON UPDATE CURRENT_TIMESTAMP(3)
+)
+	ENGINE = InnoDB
+	DEFAULT CHARSET = utf8;
+
+CREATE TABLE `location_category` (
+	`location_id`   INT          NOT NULL,
+	`category_id`   INT          NOT NULL,
+	`date_created`  TIMESTAMP(3) NOT NULL             DEFAULT CURRENT_TIMESTAMP(3),
+	`date_modified` TIMESTAMP(3) NOT NULL             DEFAULT CURRENT_TIMESTAMP(3)
+	ON UPDATE CURRENT_TIMESTAMP(3),
+	FOREIGN KEY (location_id) REFERENCES location (location_id),
+	FOREIGN KEY (category_id) REFERENCES category (category_id)
+)
+	ENGINE = InnoDB
+	DEFAULT CHARSET = utf8;
+
+CREATE TABLE `controller` (
+	`controller_id` INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	`hardware_id`   VARCHAR(255) NOT NULL,
+	`settings_id`   INT          NOT NULL,
+	`location_id`   INT          NOT NULL,
+	`date_created`  TIMESTAMP(3) NOT NULL             DEFAULT CURRENT_TIMESTAMP(3),
+	`date_modified` TIMESTAMP(3) NOT NULL             DEFAULT CURRENT_TIMESTAMP(3)
+	ON UPDATE CURRENT_TIMESTAMP(3),
+	FOREIGN KEY (settings_id) REFERENCES settings (settings_id),
+	FOREIGN KEY (location_id) REFERENCES location (location_id)
+)
+	ENGINE = InnoDB
+	DEFAULT CHARSET = utf8;
+
+
 CREATE TABLE `measuring_data` (
 	`measuring_data_id` INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	`value`             FLOAT        NOT NULL,
@@ -63,18 +77,6 @@ CREATE TABLE `measuring_data` (
 	FOREIGN KEY (controller_id) REFERENCES controller (controller_id),
 	FOREIGN KEY (unit_id) REFERENCES unit (unit_id),
 	FOREIGN KEY (location_id) REFERENCES location (location_id)
-)
-	ENGINE = InnoDB
-	DEFAULT CHARSET = utf8;
-
-CREATE TABLE `location_category` (
-	`location_id`   INT          NOT NULL,
-	`category_id`   INT          NOT NULL,
-	`date_created`  TIMESTAMP(3) NOT NULL             DEFAULT CURRENT_TIMESTAMP(3),
-	`date_modified` TIMESTAMP(3) NOT NULL             DEFAULT CURRENT_TIMESTAMP(3)
-	ON UPDATE CURRENT_TIMESTAMP(3),
-	FOREIGN KEY (location_id) REFERENCES location (location_id),
-	FOREIGN KEY (category_id) REFERENCES category (category_id)
 )
 	ENGINE = InnoDB
 	DEFAULT CHARSET = utf8;
