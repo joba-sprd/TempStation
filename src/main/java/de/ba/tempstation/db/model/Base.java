90cbeadd0ad75a692cc9a37eefcda57fbe73b8fc
Jonas Bautzmann <joba@spreadshirt.net>
2018-07-10 09:19:49 +0200

    changed timestamp formatting

--- src/main/java/de/ba/tempstation/db/model/Base.java
+++ src/main/java/de/ba/tempstation/db/model/Base.java
@@ -1,27 +1,28 @@
 package de.ba.tempstation.db.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
-import javax.persistence.Column;
-import javax.persistence.MappedSuperclass;
-import javax.persistence.PrePersist;
-import javax.persistence.PreUpdate;
+import javax.persistence.*;
 import java.util.Date;
 
 @MappedSuperclass
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Base {
 
-    @Column(name = "date_created")
+    @Column(name = "date_created", updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
     private Date dateCreated;
 
     @Column(name = "date_modified")
+    @Temporal(TemporalType.TIMESTAMP)
     private Date dateModified;
 
     @PrePersist
     protected void onCreate() {
-        dateCreated = new Date();
-        dateModified = new Date();
+        Date date = new Date();
+        dateCreated = date;
+        dateModified = date;
     }
 
     @PreUpdate
@@ -30,6 +31,7 @@ public class Base {
     }
 
     //region Getter/Setter
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     public Date getDateCreated() {
         return dateCreated;
     }
@@ -38,6 +40,7 @@ public class Base {
         this.dateCreated = dateCreated;
     }
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     public Date getDateModified() {
         return dateModified;
     }
